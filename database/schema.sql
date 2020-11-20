de2e55923737c8c8f49f7f76fa9c1e294cadf128
dumaevrinat <dumaevrinat@gmail.com>
2020-11-20 15:58:32 +0300

    add command_responses table

--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,11 @@
+-- Дежурство
 create table duties (
   id int primary key auto_increment,
   room_number int not null,
   date timestamp not null
 );
 
+-- Пользователи бота
 create table users (
   id int primary key auto_increment,
   full_name varchar(250) not null,
@@ -13,6 +15,7 @@ create table users (
   type varchar(250) not null
 );
 
+-- Запись в комнату (учебная комната, спортзал, ...)
 create table registrations (
   id int primary key auto_increment,
   user_id int not null,
@@ -24,12 +27,14 @@ create table registrations (
           on delete cascade
 );
 
+-- Команды
 create table commands (
   id int primary key auto_increment,
   name varchar(250) not null,
   is_active boolean not null
 )
 
+-- Статистика по командам
 create table command_statistics (
   id int primary key auto_increment,
   vk_id bigint not null,
@@ -41,6 +46,18 @@ create table command_statistics (
           on delete cascade
 );
 
+-- Ответы на команду
+сreate table command_responses (
+  id int primary key auto_increment,
+  response varchar(250) not null,
+  command_id int not null,
+
+  constraint command_responses_ibfk_1
+      foreign key (command_id) references commands (id)
+          on delete cascade
+);
+
+-- Синонимы, на которые реагирует бот и сопоставляет с командой
 create table command_synonyms (
   id int primary key auto_increment,
   synonym varchar(250) not null,
